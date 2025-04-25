1ea6cc5aaad0ee56a21aa781f95fa43b5758a935
Gasan <gasanalunkachev@gmail.com>
2025-04-25 23:24:39 +0300

    sgsg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react(),ghPages()],
+})
+*/
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+export default defineConfig({
   plugins: [react()],
+  base: '/Todo-App/', // <-- обязательно!
 })
